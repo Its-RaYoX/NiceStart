30b35b845e386ed5266b5c30a9a2ff864abb242d
Konrad_Alonso_Interfaces <rayo291066@gmail.com>
2025-01-13 13:36:39 +0100

    Añadido el bottom_app_bar

---
+++ app/src/main/java/com/agomez/nicestart/MainBab.java
@@ -0,0 +1,39 @@
+package com.agomez.nicestart;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainBab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_bab);
+
+        // cast al xml
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        FloatingActionButton myfab = findViewById(R.id.fab);
+
+        //click event en el  FAB
+        myfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainBab.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
